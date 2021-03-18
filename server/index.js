fc7dcd668f2087c388bd9a0301712a808e5559f9
Oliver <oliverjgodoy@gmail.com>
2021-03-18 17:56:08 -0300

    register route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 app.use(express.json()); 
 app.use(cors());
 
+//Register and login routes
+
+app.use('/auth', require('./routes/jwtAuth'))
+
 app.get('/', function(req, res) {
   res.json({message: 'Hello World'})
 });
---
+++ server/routes/jwtAuth.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const pool = require('../db')
+
+//register
+router.post('/register', async(req, res) => {
+ 
+})
+
+module.exports = router;
