94f82511dd7c11aa0d7f1142db31edc8ab48cdad
Oliver <oliverjgodoy@gmail.com>
2021-03-18 18:06:59 -0300

    check users exist on request and return 401

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -3,7 +3,23 @@ const pool = require('../db')
 
 //register
 router.post('/register', async(req, res) => {
- 
+  try {
+    //corpo da requisição
+    const { nome, sobrenome, username, senha } = req.body;
+
+    //verifica se usuario existe
+    const user = await pool.query("SELECT * FROM users WHERE username = $1", [
+      username
+    ]);
+
+    //caso exista retorna -> 401 unauthenticated
+    if(user.rows.length !== 0) {
+      return res.status(401).send("Esse usuario já existe");
+    }
+    
+  } catch (error) {
+    console.error(err.message);
+  }
 })
 
 module.exports = router;
